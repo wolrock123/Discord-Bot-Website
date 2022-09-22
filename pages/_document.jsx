7b301fa18a5dd5cbede258472bf0ae7d3406b87d
wolrock123 <112690008+wolrock123@users.noreply.github.com>
2022-09-22 07:09:14 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
+            content="How about moderating your own server with Wolrock bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@AstraBot" />
-          <meta name="twitter:creator" content="@AstraBot" />
-          <meta property="og:url" content="https://astrabot.vercel.app" />
+          <meta name="twitter:site" content="@WolrockBot" />
+          <meta name="twitter:creator" content="@WolrockBot" />
+          <meta property="og:url" content="https://Wolrockbot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Astra Bot" />
+          <meta property="og:title" content="Wolrock Bot" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,10 +26,10 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
+            content="How about moderating your own server with Wolrock bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
           />
-          <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="Astra Bot" />
+          <meta property="og:image" content="https://cdn.discordapp.com/attachments/1012301667171053608/1020798431121391707/w.png" />
+          <meta property="og:image:alt" content="Wolrock Bot" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
