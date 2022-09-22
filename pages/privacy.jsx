36beb4b227b907673c618b4acba429f8f21aece5
wolrock123 <112690008+wolrock123@users.noreply.github.com>
2022-09-22 16:19:18 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,18 +2,18 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Wolrock</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Beat</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Wolrock bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ To use the Beat bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Wolrock</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Beat</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the Wolrock Bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Every user can use the Beat Bot however they want, as long as they do not intend to harm.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using Wolrock.</p>   
+        <p>⊳ You cannot use certain commands at the same time while using Beat.</p>   
         <div className="py-3"></div>
-        <p>⊳ Wolrock bot does not save any data based on users.</p>
+        <p>⊳ Beat bot does not save any data based on users.</p>
         <div className="py-3"></div>
         <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
     
