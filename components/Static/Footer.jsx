cbd14f5f6ec37ad5e99f5599a9246dc4b5716abc
wolrock123 <112690008+wolrock123@users.noreply.github.com>
2022-09-22 16:27:33 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -109,7 +109,7 @@ export default function Footer() {
                     </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <p className="text-white text-center sm:text-left text-opacity-50">
-                            {new Date().getFullYear()} &copy; Wolrock Bot
+                            {new Date().getFullYear()} &copy; Wolrock
                         </p>
                         <div className="hidden md:flex items-center justify-center">
                                 <p className={"text-xs text-green-400"}>
@@ -117,7 +117,7 @@ export default function Footer() {
                                 </p>
                         </div>
                         <p className="text-white text-center sm:text-right text-opacity-50">
-                            {"Wolrock Bot ❤️"}
+                            {"Beat Bot ❤️"}
                         </p>
                     </div>
                 </div>
